7fa901fea8d00a915d93b79f04551c3a5ac31b6c
sqlstad <sanderstad@gmail.com>
2019-10-01 23:44:56 +0200

    Added prerequisites for SQL Server 2017 and up

--- þnameþ-Tests/Script.PostDeployment.sql
+++ þnameþ-Tests/Script.PostDeployment.sql
@@ -1,13 +1,22 @@
 ﻿/*
-Post-Deployment Script Template							
+Post-Deployment Script Template
 --------------------------------------------------------------------------------------
- This file contains SQL statements that will be appended to the build script.		
- Use SQLCMD syntax to include a file in the post-deployment script.			
- Example:      :r .\myfile.sql								
- Use SQLCMD syntax to reference a variable in the post-deployment script.		
- Example:      :setvar TableName MyTable							
-               SELECT * FROM [$(TableName)]					
+ This file contains SQL statements that will be appended to the build script.
+ Use SQLCMD syntax to include a file in the post-deployment script.
+ Example:      :r .\myfile.sql
+ Use SQLCMD syntax to reference a variable in the post-deployment script.
+ Example:      :setvar TableName MyTable
+               SELECT * FROM [$(TableName)]
 --------------------------------------------------------------------------------------
 */
 
-EXEC tSQLt.RunAll
+IF(ISNULL(CAST(SERVERPROPERTY('ProductMajorVersion') AS INT),0) >= 14)
+BEGIN
+    EXEC tSQLt.InstallExternalAccessKey;
+    EXEC master.sys.sp_executesql N'GRANT UNSAFE ASSEMBLY TO [tSQLtExternalAccessKey];';
+    EXEC sp_configure 'clr strict security', 1;
+    RECONFIGURE;
+END
+GO
+
+--EXEC tSQLt.RunAll
--- þnameþ-Tests/Script.PreDeployment.sql
+++ þnameþ-Tests/Script.PreDeployment.sql
@@ -1,18 +1,25 @@
 ﻿/*
- Pre-Deployment Script Template							
+ Pre-Deployment Script Template
 --------------------------------------------------------------------------------------
- This file contains SQL statements that will be executed before the build script.	
- Use SQLCMD syntax to include a file in the pre-deployment script.			
- Example:      :r .\myfile.sql								
- Use SQLCMD syntax to reference a variable in the pre-deployment script.		
- Example:      :setvar TableName MyTable							
-               SELECT * FROM [$(TableName)]					
+ This file contains SQL statements that will be executed before the build script.
+ Use SQLCMD syntax to include a file in the pre-deployment script.
+ Example:      :r .\myfile.sql
+ Use SQLCMD syntax to reference a variable in the pre-deployment script.
+ Example:      :setvar TableName MyTable
+               SELECT * FROM [$(TableName)]
 --------------------------------------------------------------------------------------
 */
 
 EXEC sp_configure 'clr enabled', 1;
 RECONFIGURE;
 GO
+IF(ISNULL(CAST(SERVERPROPERTY('ProductMajorVersion') AS INT),0) >= 14)
+BEGIN
+    EXEC sp_configure 'show advanced options', 1;
+    RECONFIGURE;
+    EXEC sp_configure 'clr strict security', 0;
+    RECONFIGURE;
+END
 --DECLARE @cmd NVARCHAR(MAX);
 --SET @cmd='ALTER DATABASE ' + QUOTENAME(DB_NAME()) + ' SET TRUSTWORTHY ON;';
 --EXEC(@cmd);
