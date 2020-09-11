a4860570c70561a717492be0cf6b8f26680123ba
sqlstad <sanderstad@gmail.com>
2020-09-11 16:33:16 +0200

    Removed unused files

--- Build/GitHubActions/github-actions-prerequisites.ps1
+++
@@ -1,7 +0,0 @@
-$modules = @("PSFramework", "PSModuleDevelopment", "dbatools")
-
-foreach ($module in $modules) {
-    Write-Host "Installing $module" -ForegroundColor Cyan
-    Install-Module $module -Force -SkipPublisherCheck
-    Import-Module $module -Force -PassThru
-}
