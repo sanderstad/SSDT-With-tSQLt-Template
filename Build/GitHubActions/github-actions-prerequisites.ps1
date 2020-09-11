86edac15d216947a7ad3c3832f0bf09156f1085e
sqlstad <sanderstad@gmail.com>
2020-09-11 08:02:33 +0200

    Added prerequisites file

---
+++ Build/GitHubActions/github-actions-prerequisites.ps1
@@ -0,0 +1,7 @@
+$modules = @("PSFramework", "PSModuleDevelopment", "dbatools")
+
+foreach ($module in $modules) {
+    Write-Host "Installing $module" -ForegroundColor Cyan
+    Install-Module $module -Force -SkipPublisherCheck
+    Import-Module $module -Force -PassThru
+}
